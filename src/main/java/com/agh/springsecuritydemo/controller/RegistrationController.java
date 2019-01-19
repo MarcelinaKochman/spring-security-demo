bbb8e4cb849a4af830535692fbb3c141ee15701c
Marcelina <kochmanmarcelina@gmail.com>
2019-01-20 00:10:26 +0100

    The Built-In Validation

--- src/main/java/com/agh/springsecuritydemo/controller/RegistrationController.java
+++ src/main/java/com/agh/springsecuritydemo/controller/RegistrationController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class RegistrationController {
 
     @RequestMapping(value = "/user/registration", method = RequestMethod.POST)
-    public UserDto showRegistrationForm(@RequestBody UserDto userDto) {
+    public UserDto showRegistrationForm(@RequestBody @Valid UserDto userDto) {
         return userDto;
     }
 
